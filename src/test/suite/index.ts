5356efb873872935d88f7b11095dba92fa88054c
amrbashir <amr.bashir2015@gmail.com>
2023-03-17 17:42:22 +0200

    fix: fix tests

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -4,9 +4,9 @@
 
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import { glob } from 'glob';
+import { globSync } from 'glob';
 
-export function run(): Promise<void> {
+export function run() {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -15,19 +15,18 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '..');
 
-  return glob('**/**.test.js', { cwd: testsRoot }).then((files) => {
-    // Add files to the test suite
-    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+  let files = globSync('**/**.test.js', { cwd: testsRoot });
+  // Add files to the test suite
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-    try {
-      // Run the mocha test
-      mocha.run((failures) => {
-        if (failures > 0) {
-          throw new Error(`${failures} tests failed.`);
-        }
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  });
+  try {
+    // Run the mocha test
+    mocha.run((failures) => {
+      if (failures > 0) {
+        throw new Error(`${failures} tests failed.`);
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
