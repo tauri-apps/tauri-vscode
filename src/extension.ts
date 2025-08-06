tauri-apps/tauri-vscode

commit 8f41ede1cda26ed88fe570d3030435773ae24dde

fix: install tauri-cli using cargo (#242)
* fix: install tauri-cli using cargo

Signed-off-by: Soorya U <sooryau7@gmail.com>

* chore: better error message and updated function name

* change file

---------

Signed-off-by: Soorya U <sooryau7@gmail.com>

src/extension.ts
@@ -160,7 +160,7 @@ function runTauriInit(): void {
       let onInstall = () => {};
       if (__isVueCliApp(projectPath)) {
         installCommand = 'vue add tauri';
-      } else {
+      } else if (__isNodeProject(projectPath)) {
         installCommand = __usePnpm(projectPath)
           ? 'pnpm add -D @tauri-apps/cli'
           : __useYarn(projectPath)
@@ -185,6 +185,8 @@ function runTauriInit(): void {
             noOutputWindow: true,
           });
         };
+      } else {
+        installCommand = 'cargo install tauri-cli';
       }
 
       const [command, ...args] = installCommand.split(' ');
@@ -196,7 +198,7 @@ function runTauriInit(): void {
     () => {
       const paths = __getNpmProjectsPaths();
       return paths.filter((p) => {
-        return !fs.existsSync(path.join(p, 'src-tauri'));
+        return fs.existsSync(path.join(p, 'src-tauri'));
       });
     }
   );
@@ -223,6 +225,10 @@ function runTauriBuildDebug(): void {
 function __isVueCliApp(cwd: string): boolean {
   const packageJson = __getPackageJson(cwd);
   return '@vue/cli-service' in (packageJson?.devDependencies ?? {});
+}
+
+function __isNodeProject(cwd: string): boolean {
+  return existsSync(join(cwd, 'package.json'));
 }
 
 interface PackageJson {
@@ -418,7 +424,7 @@ function __getPackageManagerCommand(projectPath: string): string | null {
 
   if (!m) {
     vscode.window.showErrorMessage(
-      "Couldn't detect package manager for current project."
+      "Couldn't detect package manager for current project. Try running Tauri: Init Command"
     );
   }
 

commit f0dd638c97fe1f4bd6bd2713c2f8872e153c0c93

fix: Fix Init command
On a workspace without Tauri (e.g. a workspace generated with
`pnpm create vite .`), the `Tauri: Init` command fails with
error message `Tauri project not found`.

That's because of a regression in #242:
https://github.com/tauri-apps/tauri-vscode/pull/242/files#diff-04bba6a35cad1c794cbbe677678a51de13441b7a6ee8592b7b50be1f05c6f626L199

The Init command should search a directory with NO subdirectory
named `src-tauri`, because Tauri is not installed yet.

src/extension.ts
@@ -199,7 +199,7 @@ function runTauriInit(): void {
     () => {
       const paths = __getNpmProjectsPaths()
       return paths.filter((p) => {
-        return fs.existsSync(path.join(p, 'src-tauri'))
+        return !fs.existsSync(path.join(p, 'src-tauri'))
       })
     }
   )